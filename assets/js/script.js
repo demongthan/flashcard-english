9b4483a0dbca254b0f174a84d27dc2cf89e2ee8f
Nguyen Nam <nam.nguyenbku00@gmail.com>
2024-11-26 08:05:11 +0700

    nam commit

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+const cardContainer = document.querySelector('.card__container');
+
+console.log(wordData)
+
+wordData.forEach(word => {
+    const article = document.createElement('article');
+    article.className = 'card__article';
+
+    article.innerHTML = `
+        <img src="${word.img}" alt="image" class="card__img">
+        <div class="card__data">
+            <span class="card__description">${word.translate}</span>
+            <h2 class="card__title">${word.vocabulary}</h2>
+            <div class="card__info">
+                <p class="card__info__description">${word.transcription}</p>
+                <div class="card__info__audio" id="${"AudioDiv"+word.id}">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                    </svg>
+                    <audio id="${"Audio"+word.id}" src="${word.audio}"></audio>
+                </div>
+            </div>
+            <a href="${word.url}" class="card__button" target="_blank">Read More</a>
+        </div>
+    `;
+
+    cardContainer.appendChild(article);
+
+    const audioDiv = document.getElementById('AudioDiv'+word.id);
+    const audio = document.getElementById('Audio'+word.id);
+
+    audioDiv.addEventListener('click', () => {
+        audio.play();
+    });
+});
---
+++ assets/js/word-data.js
@@ -0,0 +1,317 @@
+const wordData=[
+    {
+        id:1,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/ability.jpg",
+        vocabulary:"ability (n)",
+        translate:"khả năng",
+        transcription:"/əˈbɪlət̮i/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/american_english/ability_1",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/ability__us_2.mp3"
+    },
+    {
+        id:2,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/able.avif",
+        vocabulary:"able (adj)",
+        translate:"có thể",
+        transcription:"/ˈeɪbl/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/able_1?q=able",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/able__gb_1.mp3"
+    },
+    {
+        id:3,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/apply.avif",
+        vocabulary:"apply (v)",
+        translate:"áp dụng, nộp đơn",
+        transcription:"/əˈplaɪ/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/apply?q=apply",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/apply__gb_1.mp3"
+    },
+    {
+        id:4,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/applicant.avif",
+        vocabulary:"applicant (n)",
+        translate:"người nộp đơn",
+        transcription:"/ˈæplɪkənt/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/applicant?q=applicant",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/applicant__gb_1.mp3"
+    },
+    {
+        id:5,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/application.avif",
+        vocabulary:"application (n)",
+        translate:"đơn xin việc, ứng dụng",
+        transcription:"/ˌæplɪˈkeɪʃn/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/application?q=application",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/application__gb_2.mp3"
+    },
+    {
+        id:6,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/background.avif",
+        vocabulary:"background (n)",
+        translate:"nền tảng, xuất thân",
+        transcription:"/ˈbækɡraʊnd/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/background?q=background",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/background__gb_1.mp3"
+    },
+    {
+        id:7,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/bereadyfor.avif",
+        vocabulary:"be ready for (v)",
+        translate:"sẵn sàng cho",
+        transcription:"/bi/ /ˈredi/ /fə(r)/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/ready_1?q=ready",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/ready__gb_1.mp3"
+    },
+    {
+        id:8,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/call.avif",
+        vocabulary:"call in (v)",
+        translate:"gọi điện (đến nơi làm việc)",
+        transcription:"/kɔːl/ ",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/call-in_1?q=call+in",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/call__gb_3.mp3"
+    },
+    {
+        id:9,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/confidence.avif",
+        vocabulary:"confidence (n)",
+        translate:"sự tự tin",
+        transcription:"/ˈkɒnfɪdəns/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/confidence?q=confidence",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/xconfidence__gb_1.mp3"
+    },
+    {
+        id:10,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/confidently.avif",
+        vocabulary:"confidently (adv)",
+        translate:"một cách tự tin",
+        transcription:"/ˈkɒnfɪdəntli/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/confidently?q=confidently",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/xconfidently__gb_1.mp3"
+    },
+    {
+        id:11,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/confident.avif",
+        vocabulary:"confident (adj)",
+        translate:"tự tin",
+        transcription:"/ˈkɒnfɪdənt/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/confident?q=confident",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/xconfident__gb_1.mp3"
+    },
+    {
+        id:12,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/constant.avif",
+        vocabulary:"constant (adj)",
+        translate:"liên tục, không đổi",
+        transcription:"/ˈkɒnstənt/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/constant_1?q=constant",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/xconstant__gb_2.mp3"
+    },
+    {
+        id:13,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/constantly.avif",
+        vocabulary:"constantly (adv)",
+        translate:"liên tục",
+        transcription:"/ˈkɒnstəntli/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/constantly?q=constantly",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/xconstantly__gb_1.mp3"
+    },
+    {
+        id:14,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/constance.avif",
+        vocabulary:"constance (n)",
+        translate:"sự kiên định",
+        transcription:"/ˈkɒnstəns/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/constance?q=constance",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/xconstance_1_gb_1.mp3"
+    },
+    {
+        id:15,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/expert.avif",
+        vocabulary:"expert (n) | expert (adj)",
+        translate:"chuyên gia | thành thạo, lão luyện",
+        transcription:"/ˈekspɜːt/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/expert_1?q=expert",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/expert__gb_2.mp3"
+    },
+    {
+        id:16,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/follow.avif",
+        vocabulary:"follow up (v)",
+        translate:"theo dõi, tiếp tục (liên lạc lại)",
+        transcription:"/ˈfɒləʊ/ /ʌp/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/follow-up_1?q=follow+up",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/follow__gb_1.mp3"
+    },
+    {
+        id:17,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/hesitant.avif",
+        vocabulary:"hesitant (adj)",
+        translate:"do dự, ngập ngừng",
+        transcription:"/ˈhezɪtənt/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/hesitant?q=hesitant",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/hesitant__gb_1.mp3"
+    },
+    {
+        id:18,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/hensitantly.avif",
+        vocabulary:"hesitantly (adv)",
+        translate:"một cách do dự",
+        transcription:"/ˈhezɪtəntli/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/hesitantly?q=hesitantly",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/hesitantly__gb_1.mp3"
+    },
+    {
+        id:19,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/hesitation_1.avif",
+        vocabulary:"hesitation (n)",
+        translate:"sự do dự",
+        transcription:"/ˌhezɪˈteɪʃn/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/hesitation?q=hesitation",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/hesitation__gb_1.mp3"
+    },
+    {
+        id:20,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/present.avif",
+        vocabulary:"present (v)",
+        translate:"trình bày",
+        transcription:"/ˈpreznt/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/present_1?q=present",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/present__gb_4.mp3"
+    },
+    {
+        id:21,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/presentation_1.avif",
+        vocabulary:"presentation (n)",
+        translate:"bài thuyết trình",
+        transcription:"/ˌpreznˈteɪʃn/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/presentation?q=presentation",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/presentation__gb_2.mp3"
+    },
+    {
+        id:22,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/presentable.avif",
+        vocabulary:"presentable (adj)",
+        translate:"ưa nhìn, lịch sự",
+        transcription:"/prɪˈzentəbl/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/presentable?q=presentable",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/presentable__gb_1.mp3"
+    },
+    {
+        id:23,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/weakly.avif",
+        vocabulary:"weakly (adv)",
+        translate:"yếu ớt",
+        transcription:"/ˈwiːkli/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/weakly?q=weakly",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/weakly__gb_1.mp3"
+    },
+    {
+        id:24,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/fork.avif",
+        vocabulary:"fork (n)",
+        translate:"cái nĩa",
+        transcription:"/fɔːk/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/fork_1?q=fork",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/fork__gb_2.mp3"
+    },
+    {
+        id:25,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/ladles.jpg",
+        vocabulary:"ladles (n)",
+        translate:"vá múc canh",
+        transcription:"/ˈleɪdl/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/ladle_1?q=ladles",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/ladle__gb_1.mp3"
+    },
+    {
+        id:26,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/napkin.avif",
+        vocabulary:"napkin (n)",
+        translate:"khăn ăn",
+        transcription:"/ˈnæpkɪn/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/napkin?q=napkin",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/napkin__gb_1.mp3"
+    },
+    {
+        id:27,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/reservation.avif",
+        vocabulary:"reservation (n)",
+        translate:"đặt bàn",
+        transcription:"/ˌrezəˈveɪʃn/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/reservation?q=reservation",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/reservation__gb_2.mp3"
+    },
+    {
+        id:28,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/appetizer.avif",
+        vocabulary:"appetizer (n)",
+        translate:"Khai vị",
+        transcription:"/ˈæpɪtaɪzə(r)/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/appetizer?q=appetizer",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/appetizer__gb_1.mp3"
+    },
+    {
+        id:29,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/maincourse.avif",
+        vocabulary:"main course (n)",
+        translate:"món chính",
+        transcription:"/ˌmeɪn ˈkɔːs/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/main-course?q=main+course",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/main_course_1_gb_1.mp3"
+    },
+    {
+        id:30,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/desert.avif",
+        vocabulary:"desert (n)",
+        translate:"món tráng miệng",
+        transcription:"/dɪˈzɜːt/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/dessert?q=dessert",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/dessert__gb_1.mp3"
+    },
+    {
+        id:31,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/banquet.avif",
+        vocabulary:"banquet (n)",
+        translate:"bữa tiệc",
+        transcription:"/ˈbæŋkwɪt/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/banquet?q=banquet",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/banquet__gb_2.mp3"
+    },
+    {
+        id:32,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/barista.avif",
+        vocabulary:"barista (n)",
+        translate:"người pha chế cafe",
+        transcription:"/bəˈriːstə/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/barista?q=barista",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/barista__gb_1.mp3"
+    },
+    {
+        id:33,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/bartender.jpg",
+        vocabulary:"bartender (n)",
+        translate:"người pha chế",
+        transcription:"/ˈbɑːtendə(r)/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/bartender?q=bartender",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/bartender__gb_1.mp3"
+    },
+    {
+        id:34,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/cury.avif",
+        vocabulary:"curry (n)",
+        translate:"cà ri",
+        transcription:"/ˈkʌri/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/curry_1?q=curry",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/curry__gb_1.mp3"
+    },
+    {
+        id:35,
+        img:"https://personal-project.b-cdn.net/flashcard-english/image/fishsauce.avif",
+        vocabulary:"fish sauce (n)",
+        translate:"nước mắm",
+        transcription:"/ˈkʌri/",
+        url:"https://www.oxfordlearnersdictionaries.com/definition/english/curry_1?q=curry",
+        audio:"https://personal-project.b-cdn.net/flashcard-english/audio/curry__gb_1.mp3"
+    }
+]
